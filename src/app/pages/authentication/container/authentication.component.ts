c05ed8d1a2f09cf6ebd5cda1ccec12ef0ff790e3
satish_ipedis <satish@ipedis.com>
2021-10-02 11:04:32 +0400

    EV-folder-structure - done - folder structure -done - fonts - done - angular.json - adapted

---
+++ src/app/pages/authentication/container/authentication.component.ts
@@ -0,0 +1,14 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+
+@Component({
+  selector: 'rs-authentication',
+  templateUrl: './authentication.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+export class AuthenticationComponent {
+
+
+
+  constructor() {}
+}
---
+++ src/app/pages/dashboard/container/dashboard.component.ts
@@ -0,0 +1,11 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+
+@Component({
+  selector: 'ev-dashboard',
+  templateUrl: './dashboard.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+export class DashboardComponent {
+  
+}
